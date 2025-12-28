fb5d437d4b0acba5c1a06d8e625fc582a3f0e5df
shivikarani <ranishivika7505@gmail.com>
2025-12-28 11:57:22 +0530

    first28

--- typing_speed_app/urls.py
+++ typing_speed_app/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "Typing Speed App Admin"
+admin.site.site_header = "Typing Speed Test Website Admin"
 admin.site.site_title = "Typing Speed App Admin Portal"
-admin.site.index_title = "Welcome to Typing Speed App"
+admin.site.index_title = "Typing Speed Test Website- Logged in as admin(admin)"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
